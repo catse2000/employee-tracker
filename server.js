135b8ae1df5ccf9787dba6bb8a1d026ad9215d3f
catse2000 <catse2000@yahoo.com>
2020-10-18 23:29:29 -0500

    added ability to add Departments

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require("inquirer");
+const { connect } = require('http2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -51,9 +52,11 @@ const connection = mysql.createConnection({
                 break;
             case 'Add a Department':
                 console.log("You chose to 'Add a Department'");
+                addDepartment();
                 break;
             case 'Add a Role':
                 console.log("You chose to 'Add a Role'");
+                addRole();
                 break;
             case 'Add an Employee':
                 console.log("you chose to 'Add an Employee'");
@@ -114,3 +117,58 @@ function displayEmployees() {
     });
 };
 
+function addDepartment() {
+    inquirer.prompt([
+        {
+            name: "dept_name",
+            message: 'What department would you like to add?',
+            type: "input"
+        }
+    ])
+    .then(({dept_name}) => {
+        const sql = `INSERT INTO department (dept_name)
+                    VALUES (?)`;
+        const params = [dept_name];
+        connection.query(sql, params, (err, res) => {
+            if (err) throw err;
+            console.log(`Department ${dept_name} added!`);
+            displayDepartments();
+        })
+    })
+    .then(() => {
+        mainMenu()
+    });
+};
+
+function addRole() {
+    inquirer.prompt([
+        {
+            name: "role_title",
+            message: 'What role would you like to add?',
+            type: 'input'
+        },
+        {
+            name: "role_salary",
+            message: "What Salary does this role have?",
+            type: 'input'
+        },
+        {
+            name: "role_department_id",
+            message: "What is the ID for the department this role belongs to?",
+            type: 'input'
+        }
+    ])
+    .then(({role_title, role_salary, role_department_id}) => {
+        const sql = `INSERT INTO emp_role (title, salary, department_id)
+                    VALUES (?, ?, ?)`;
+        const params = [role_title, role_salary, role_department_id];
+        connection.query(sql, params, (err, res) => {
+            if (err) throw err;
+            console.log(`Role ${role_name} added!`);
+            displayRoles();
+        })
+    })
+    .then(() => {
+        mainMenu()
+    });
+};
